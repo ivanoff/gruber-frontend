16f6afa23759b44b5e6a964fd8a3f536211e2567
Dimitry Ivanov <2@ivanoff.org.ua>
2018-01-18 09:37:32 +0200

    move front to up

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.json([ {name: 'test', value: 13}, {name: 'test2', value: 14} ]))
-
-app.listen(3038, () => console.log('Example app listening on port 3038!'))
